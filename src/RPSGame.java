bbe76b907d0323c7407e13c8ad52ce7d7d779cd9
menukfernando <100038759+menukfernando@users.noreply.github.com>
2024-03-20 12:21:53 +0530

    computer's move logic

--- src/RPSGame.java
+++ src/RPSGame.java
@@ -5,7 +5,5 @@ public class RPSGame {
     public static void main(String[] args) {
         String[] rps = {"rock", "paper", "scissor"};
         String computerMove = rps[new Random().nextInt(rps.length)];
-
-
     }
 }
